72422ffb4a81e9c402efe833dd8f182015ea4d0d
darryltestcode <darryltestcode@gmail.com>
2021-10-10 16:13:50 +0100

    reducer added

---
+++ src/reducer.js
@@ -0,0 +1,24 @@
+const initialState = {
+  bestLines: [],
+  board: [],
+  nextStates: [],
+  moves: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'STATE':
+      return { ...state, ...action.payload };
+    case 'MOVE':
+      return (({ board, nextStates, description }) => ({
+        ...state,
+        board,
+        nextStates,
+        moves: [...state.moves, ...(description ? [description] : [])],
+      }))(action.payload);
+    case 'BEST_LINES':
+      return { ...state, bestLines: action.payload };
+  }
+
+  return state;
+};
